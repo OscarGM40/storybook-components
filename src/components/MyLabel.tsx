3fc2767f2c06bce8283ee5f06b3c1b66773a9640
Oscar <oscargm28@hotmail.com>
2022-02-23 20:12:11 +0100

    feat: added css modules and augmented compatibility

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -36,3 +36,4 @@ const MyLabel = ({
 };
 
 export { MyLabel };
+export default MyLabel;
---
+++ src/declarations.d.ts
@@ -0,0 +1,3 @@
+/* este archivo soluciona el usar CSSModules y su importación posterior usando import XXX from 'file.css' en vez de import 'file.css' */
+
+declare module '*.css' {  }
